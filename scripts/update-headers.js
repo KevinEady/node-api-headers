1668ba1c46571a2082486244a94fb6e681fc32dc
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2023-01-03 15:19:09 +0100

    Squashed commit of the following:
    commit b9a57c3721c8be26c76a5e2777e05ed3886a938b
    Author: Kevin Eady <8634912+KevinEady@users.noreply.github.com>
    Date:   Tue Jan 3 15:18:03 2023 +0100
    
        Address review comments
        - Add 'use strict'
        - Make write-symbols main() async
    
    commit eb2f62898f4d6f3eb34ec4f5e133f972d1bb0806
    Author: Kevin Eady <8634912+KevinEady@users.noreply.github.com>
    Date:   Sat Dec 31 04:28:19 2022 +0100
    
        Add GitHub Actions sync workflow
    
    commit 0834c30961cec82d25cb0736ec1216b18a1d3cfd
    Author: Kevin Eady <8634912+KevinEady@users.noreply.github.com>
    Date:   Fri Dec 30 23:47:57 2022 +0100
    
        Use latest release from nodejs.org releases
    
    commit 8e693f75d5dbf726bf559b04834efa0a0f12112b
    Author: Kevin Eady <8634912+KevinEady@users.noreply.github.com>
    Date:   Fri Dec 30 03:28:45 2022 +0100
    
        Use current node major version branch for headers
    
    commit 63c61f08d5f42738546a95ed307059a93743abe9
    Author: Kevin Eady <8634912+KevinEady@users.noreply.github.com>
    Date:   Fri Dec 30 03:01:04 2022 +0100
    
        Add helper scripts
        - update-headers.js: Fetch latest headers from nodejs/node
        - write-symbols.js: Use clang to process headers to create symbols.js

---
+++ scripts/update-headers.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const { createWriteStream } = require('fs');
+const { Readable } = require('stream');
+const { finished } = require('stream/promises');
+const { resolve } = require('path');
+const { parseArgs } = require('util')
+
+async function getLatestReleaseVersion() {
+    const response = await fetch('https://nodejs.org/download/release/index.json');
+    const json = await response.json();
+    return json[0].version;
+}
+
+async function main() {
+    const { values: { tag, verbose } } = parseArgs({
+        options: {
+            tag: {
+                type: "string",
+                short: "t",
+                default: await getLatestReleaseVersion()
+            },
+            verbose: {
+                type: "boolean",
+                short: "v",
+            },
+        },
+    });
+
+    console.log(`Update headers from nodejs/node tag ${tag}`);
+
+    const files = ['js_native_api_types.h', 'js_native_api.h', 'node_api_types.h', 'node_api.h'];
+
+    for (const filename of files) {
+        const url = `https://raw.githubusercontent.com/nodejs/node/${tag}/src/${filename}`;
+        const path = resolve(__dirname, '..', 'include', filename);
+
+        if (verbose) {
+            console.log(`  ${url} -> ${path}`);
+        }
+
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Fetch of ${url} returned ${response.status} ${response.statusText}`);
+        }
+
+        const stream = createWriteStream(path);
+        await finished(Readable.fromWeb(response.body).pipe(stream));
+    }
+}
+
+main().catch(e => {
+    console.error(e);
+    process.exitCode = 1;
+});
